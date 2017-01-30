46dd697b4748ba355f647f02d3625ebd3d2b3014
Tobias Dalå <tobias@dalaa.com>
2017-01-30 11:09:46 +0100

    adding factorissing

--- python/python math/2gradsnew.py
+++ python/python math/2gradsnew.py
@@ -1,11 +1,12 @@
+# coding=utf-8
 import math
 f = open('results of 2grads.txt','w')
 a = input("a : ")
 b = input("b : ")
 c = input("c : ")
-f.write("a : {} \n".format(a))
-f.write("b : {}\n".format(b))
-f.write("c : {}\n".format(c))
+# f.write("a : {} \n".format(a))
+# f.write("b : {}\n".format(b))
+# f.write("c : {}\n".format(c))
 d = b*b-(4*a*c)
 f.write("\nd : {}\n".format(d))
 print
@@ -18,6 +19,7 @@ if d == 0.0 or 0:
     print "x : " + str(x)
     f.write("x : {}".format(x))
     f.write("\nd = ({}*{})-(4*{}*{})\n\n".format(b,b,a,c))
+    f.write("faktoriseret: \na(x-{})²\n".format(x))
 
 if d < 0:
     print "there is no x."
@@ -35,6 +37,7 @@ if d > 0:
     f.write("\n")
     f.write("x{} : {}".format("2", x2))
     f.write("\nd = ({}*{})-(4*{}*{})\n\n".format(b,b,a,c))
+    f.write("faktoriseret: \na(x-{})(x-{})\n".format(x1,x2))
 
 toppunktx = ((-b)/(2*a))
 toppunkty = ((-d)/(4*a))
@@ -45,7 +48,7 @@ f.write("T({},{})".format(toppunktx,toppunkty))
 ##########################################################
 #ULIGHEDER
 ##########################################################
-# 
+#
 # unqeual = raw_input("Is equation an unequality? y/n : ")
 #
 # if unqeual == "y":
